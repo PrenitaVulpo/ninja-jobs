43657e2ff5af7692e9ac15859dba54d2dbb9701d
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-04-23 04:06:03 -0300

    modified home and about pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fugit veniam, natus sed incidunt, reiciendis asperiores excepturi dolorum sint debitis consequuntur, iusto rerum placeat vel quam officia. Perferendis vitae minima distinctio obcaecati, enim error rerum ea odit fugit dolore nisi magni reprehenderit porro? Labore pariatur incidunt, minus obcaecati possimus placeat.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum fugit veniam, natus sed incidunt, reiciendis asperiores excepturi dolorum sint debitis consequuntur, iusto rerum placeat vel quam officia. Perferendis vitae minima distinctio obcaecati, enim error rerum ea odit fugit dolore nisi magni reprehenderit porro? Labore pariatur incidunt, minus obcaecati possimus placeat.</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae quisquam, ratione explicabo doloribus ipsam est sit voluptates? Fugiat nostrum consequuntur accusamus optio facere ducimus magnam numquam sint nesciunt aspernatur a deserunt odio incidunt quidem iusto, earum ut doloribus et ab vitae necessitatibus cumque illo ullam obcaecati. Quisquam, possimus in.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique molestiae quisquam, ratione explicabo doloribus ipsam est sit voluptates? Fugiat nostrum consequuntur accusamus optio facere ducimus magnam numquam sint nesciunt aspernatur a deserunt odio incidunt quidem iusto, earum ut doloribus et ab vitae necessitatibus cumque illo ullam obcaecati. Quisquam, possimus in.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
